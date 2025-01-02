85c51b8a8d91a2dad862eeb1387b6e8bcc17258a
Đỗ Ngọc Tuyền <dongoctuyenn@gmail.com>
2025-01-02 22:50:01 +0700

    fix race condition

---
+++ race_condition_fix.py
@@ -0,0 +1,37 @@
+import concurrent.futures
+import logging
+import threading
+import time
+
+
+class FakeDatabase:
+
+    def __init__(self):
+        self.value = 0
+        self._lock = threading.Lock()
+
+    def update(self, name):
+        logging.info("Thread %s: starting update", name)
+        logging.debug("Thread %s about to lock", name)
+        with self._lock:
+            logging.debug("Thread %s has lock", name)
+            local_copy = self.value
+            local_copy += 1
+            time.sleep(0.1)
+            self.value = local_copy
+            logging.debug("Thread %s about to release lock", name)
+        logging.debug("Thread %s after release", name)
+        logging.info("Thread %s: finishing update", name)
+
+
+if __name__ == '__main__':
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.DEBUG, datefmt="%H:%M:%S")
+
+    database = FakeDatabase()
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        for i in range(2):
+            executor.submit(database.update, i)
+
+    logging.info("Testing update. Ending value is %d.", database.value)

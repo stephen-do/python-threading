b56f0c749463dd4ead5da96ea8b340faeff8996a
Đỗ Ngọc Tuyền <dongoctuyenn@gmail.com>
2025-01-02 22:06:38 +0700

    multiple threads

---
+++ README.md
@@ -0,0 +1,31 @@
+
+# Python Multithreading
+
+
+
+
+## Installation
+
+Create virtual environment
+
+```bash
+  python -m venv .venv
+  cd .venv\Scripts
+  activate
+```
+## Run Locally
+
+Clone the project
+
+```bash
+  python file_name.py
+```
+
+## Authors
+
+- [@Đỗ Ngọc Tuyền](https://www.github.com/stephen-do)
+
+
+## Tech Stack
+
+Python
--- single_thread.py
+++ single_thread.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
     logging.info("Main    : before creating thread")
-    x = threading.Thread(target=func, args=(1,))
+    x = threading.Thread(target=func, args=(1,), daemon=True)
     logging.info("Main    : before running thread")
     x.start()
     logging.info("Main    : wait for the thread to finish")
